bac1c006f1f80bfeec1ce8ad94c6e4ae05ec7a2c
Duke <duc@silentium.io>
2025-03-14 16:14:42 +0700

    feat: added extra package

---
+++ packages/extra/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/client.ts", "src/server.ts", "src/bun/index.ts", "src/meta/http.ts", "src/standardschema.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  clean: true,
+  minify: true,
+  target: ["node18", "es2022"],
+  outDir: "dist",
+  sourcemap: true,
+});
